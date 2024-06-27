b5d87bfc3df00c2d549882754fbd156042050f98
Arwyn17 <joaopedro14702@gmail.com>
2024-06-27 19:09:14 -0300

    Apenas um teste para entender o Git GUI.
    É bom pois vai facilitar meu trabalho

---
+++ ProjetoFinal/teste.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <iostream>
+#include <set>
+#include <list>
+
+using namespace std;
+
+class Jogos {
+  private:
+  int lig4_tabuleiro[7][4];
+
+public:
+  int retornar_id() {
+  }
+
+}
