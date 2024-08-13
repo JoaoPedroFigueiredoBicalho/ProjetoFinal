c27ff07541e6e5c472e7dba012c46d2eeb8399ba
kaue19santos <kaue10166@gmail.com>
2024-08-13 12:29:01 -0300

    coloquei o char jogada para comecar com o valor inicial X, e alterei o ler jogada do lig4

--- ProjetoFinal/lig4.cpp
+++ ProjetoFinal/lig4.cpp
@@ -5,12 +5,19 @@ void Lig4::lerjogada(int jogadaColuna, char jogada)
     if (jogadaColuna < 0 || jogadaColuna >= tamanho) {
         std::cout << "Coluna inválida!" << std::endl;
         return;
-    }
+    } else {
 
-    for (int linha = 0; linha <= tamanho; linha++) {
+    for (int linha = tamanho; linha >= 0; linha--) {
         if (tabuleiro[linha][jogadaColuna] == ' ') {
             tabuleiro[linha][jogadaColuna] = jogada;
-        }
+        } 
     }
+    }
+    if (jogada = 'X') {
+        jogada = 'O';
+    } else {
+        jogada = 'X';
+    } 
+    //colocar o tratamento de excecao para coluna cheia
 }
 
