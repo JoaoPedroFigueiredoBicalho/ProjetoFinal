962e4e84bfbacdcafa3c011a8165361aa8c615d3
Arwyn17 <joaopedro14702@gmail.com>
2024-08-13 12:11:44 -0300

    Progresso na atualização de casas do Reversi

--- ProjetoFinal/reversi.hpp
+++ ProjetoFinal/reversi.hpp
@@ -3,7 +3,13 @@
 #include "boardLogic.hpp"
 class Reversi : public boardLogic
 {
+private:
+  const char Jogador1 = 'X';
+  const char Jogador2 = 'O';
+
 public:
   void lerjogada(int jogadaLinha, int jogadaColuna, char jogada);
+  void checar_jogada_valida(int linha, int coluna);
+  void checar_casas_viradas(int direcao);
 };
 #endif
