1fd0d53367cd5fbb7722d137bd7066859f5c9bd0
Leonardo Chiari Maciel <140858892+LeonardoChiariMaciel@users.noreply.github.com>
2024-08-15 11:31:13 -0300

    Update boardLogic.cpp
    11h30 cpp

--- ProjetoFinal/src/boardLogic.cpp
+++ ProjetoFinal/src/boardLogic.cpp
@@ -1,47 +1,40 @@
 #include "boardLogic.hpp"
+#include <iostream>
 
-void boardLogic::lerjogada(int jogadaLinha, int jogadaColuna, char jogada)
-{
-}
+void boardLogic::lerjogada(int jogadaLinha, int jogadaColuna, char jogada) {}
 
-void boardLogic::set_tabuleiro(int linha, int coluna, char jogada)
-{
+void boardLogic::set_tabuleiro(int linha, int coluna, char jogada) {
   tabuleiro[linha][coluna] = jogada;
 }
 
-void boardLogic::inicializar_tabuleiro(int tamanho)
-{
+// mudança
+std::vector<std::vector<char>> &boardLogic::get_tabuleiro() {
+  return tabuleiro;
+};
+
+void boardLogic::inicializar_tabuleiro(int tamanho) {
   this->tamanho = tamanho;
+  // Redimensiona o vetor 'tabuleiro' para 'tamanho x tamanho' e inicializa com
+  // espaços vazios
   tabuleiro.resize(tamanho, std::vector<char>(tamanho, ' '));
 }
 
-void boardLogic::imprimir_tabuleiro()
-{
+void boardLogic::imprimir_tabuleiro() {
   std::cout << "#########" << std::endl;
-  for (int i = 0; i < tabuleiro.size(); i++)
-  {
+  for (int i = 0; i < tabuleiro.size(); i++) {
     std::cout << std::endl;
-    for (int j = 0; j < tabuleiro[0].size(); j++)
-    {
+    for (int j = 0; j < tabuleiro[i].size(); j++) {
       std::cout << "|" << tabuleiro[i][j];
-      if (j == tabuleiro[0].size() - 1)
+      if (j == tabuleiro[i].size() - 1)
         std::cout << "|";
     }
   }
-  std::cout << std::endl
-            << "#########" << std::endl;
+  std::cout << std::endl << "#########" << std::endl;
 }
 
-int boardLogic::get_tamanho()
-{
-  return tamanho;
-}
-void boardLogic::set_tamanho(int tamanho)
-{
-  this->tamanho = tamanho;
-}
+char boardLogic::get_jogada_atual() const { return jogada; }
 
-std::vector<std::vector<char>> &boardLogic::get_tabuleiro()
-{
-  return tabuleiro;
-};
+void boardLogic::alternar_jogador() { jogada = (jogada == 'X') ? 'O' : 'X'; }
+
+// pro reversi
+int boardLogic::get_tamanho() { return tamanho; }
