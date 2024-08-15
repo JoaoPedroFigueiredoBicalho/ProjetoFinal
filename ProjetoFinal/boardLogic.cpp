fce8df9351a008a1b3f35db45323f1505964bbdb
Rafael <neubanerrafael@gmail.com>
2024-08-15 16:54:53 -0300

    .

---
+++ ProjetoFinal/boardLogic.cpp
@@ -0,0 +1,33 @@
+#include "boardLogic.hpp"
+
+void boardLogic::lerjogada(int jogadaLinha, int jogadaColuna, char jogada)
+{
+}
+
+void boardLogic::inicializar_tabuleiro(int tamanho)
+{
+  for (int i = 0; i < tamanho; i++)
+  {
+    for (int j = 0; j < tamanho; j++)
+    {
+      tabuleiro[i][j] = ' ';
+    }
+  }
+}
+
+void boardLogic::imprimir_tabuleiro()
+{
+  std::cout << "#########" << std::endl;
+  for (int i = 0; i < tabuleiro.size(); i++)
+  {
+    std::cout << std::endl;
+    for (int j = 0; j < tabuleiro[0].size(); j++)
+    {
+      std::cout << "|" << tabuleiro[i][j];
+      if (j == tabuleiro[0].size() - 1)
+        std::cout << "|";
+    }
+  }
+  std::cout << "#########" << std::endl;
+}
+AQUI É GALO
