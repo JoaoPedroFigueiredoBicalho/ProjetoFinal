7c07a51ba100825e3af1e37f7363c3940e67e529
Arwyn17 <joaopedro14702@gmail.com>
2024-08-16 10:44:20 -0300

    Atualização do imprimir tabuleiro

--- ProjetoFinal/src/boardLogic.cpp
+++ ProjetoFinal/src/boardLogic.cpp
@@ -3,33 +3,42 @@
 
 void boardLogic::lerjogada(int jogadaLinha, int jogadaColuna, char jogada) {}
 
-void boardLogic::set_tabuleiro(int linha, int coluna, char jogada) {
+void boardLogic::set_tabuleiro(int linha, int coluna, char jogada)
+{
   tabuleiro[linha][coluna] = jogada;
 }
 
 // mudança
-std::vector<std::vector<char>> &boardLogic::get_tabuleiro() {
+std::vector<std::vector<char>> &boardLogic::get_tabuleiro()
+{
   return tabuleiro;
 };
 
-void boardLogic::inicializar_tabuleiro(int tamanho) {
+void boardLogic::inicializar_tabuleiro(int tamanho)
+{
   this->tamanho = tamanho;
   // Redimensiona o vetor 'tabuleiro' para 'tamanho x tamanho' e inicializa com
   // espaços vazios
   tabuleiro.resize(tamanho, std::vector<char>(tamanho, ' '));
 }
 
-void boardLogic::imprimir_tabuleiro() {
-  std::cout << "#########" << std::endl;
-  for (int i = 0; i < tabuleiro.size(); i++) {
-    std::cout << std::endl;
-    for (int j = 0; j < tabuleiro[i].size(); j++) {
+void boardLogic::imprimir_tabuleiro()
+{
+  std::cout << "##############" << std::endl;
+  std::cout << " |1|2|3|4|5|6|";
+  for (int i = 0; i < tabuleiro.size(); i++)
+  {
+    std::cout << std::endl
+              << i + 1;
+    for (int j = 0; j < tabuleiro[i].size(); j++)
+    {
       std::cout << "|" << tabuleiro[i][j];
       if (j == tabuleiro[i].size() - 1)
         std::cout << "|";
     }
   }
-  std::cout << std::endl << "#########" << std::endl;
+  std::cout << std::endl
+            << "##############" << std::endl;
 }
 
 char boardLogic::get_jogada_atual() const { return jogada; }
