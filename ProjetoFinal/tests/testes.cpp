edace6ad5c61d8dd39972850657ed72aeabd76a5
Kauê <137323311+kaue19santos@users.noreply.github.com>
2024-08-17 15:08:07 -0300

    Criando a estrutura inicial do testes.cpp

--- ProjetoFinal/tests/testes.cpp
+++ ProjetoFinal/tests/testes.cpp
@@ -1,1 +1,15 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
+#include "lig4.cpp"
+
+TEST_CASE("Teste de verificação da função lerjogada") {
+    Lig4 jogo;
+
+    //Verificando se o usuário inseriu um número negativo
+    CHECK(jogo.lerjogada(-2) == false);
+
+    //Verificando se o usuário inseriu algum número maior que o tabuleiro
+    
+}
+
 
